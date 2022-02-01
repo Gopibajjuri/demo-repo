161e5e9955a9effd663d7602581379a438acd936
Gopibajjuri <gopibajjuri2001@gmail.com>
2022-02-01 18:34:09 +0530

    first commit

---
+++ src/main/java/com/in28minutes/springboot/web/service/LoginService.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.web.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginService {
+	
+	public boolean validateUser(String userid, String password) {
+		
+		return userid.equalsIgnoreCase("in28Minutes") && password.equalsIgnoreCase("dummy");
+	}
+}
